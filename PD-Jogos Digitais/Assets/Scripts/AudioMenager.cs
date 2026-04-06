68b7eeb4335630898892e51090b0c4c677793fc7
Lauan13 <lauan.lucas@escolar.ifrn.edu.br>
2026-04-06 08:19:08 -0300

    Criando um nova cena _Boot

---
+++ PD-Jogos Digitais/Assets/Scripts/AudioMenager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AudioMenager : MonoBehaviour
+{
+    public static AudioMenager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
